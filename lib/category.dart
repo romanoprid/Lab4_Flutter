c6949bbeb643665446bb0ac2e7b096afbbf51f93
unknown <romanoprisk557@gmail.com>
2021-11-09 13:07:31 +0200

    Id was added

--- lib/category.dart
+++ lib/category.dart
@@ -1,13 +1,13 @@
 import "package:flutter/material.dart";
 
 class Category {
+  int id;
   String name;
   int price;
-  Icon icon;
   String imgName;
   List<Category> subCategories;
   Category({
-    required this.icon,
+    required this.id,
     required this.name,
     required this.price,
     required this.imgName,
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,29 +1,28 @@
-import 'package:flutter/material.dart';
 import 'package:lab4/category.dart';
 
 class Utils {
   static List<Category> getMockedCategory() {
     return [
       Category(
-          icon: Icon(Icons.ac_unit),
+          id: 1,
           name: "Item Name",
           price: 9000,
           imgName: "assets/Chair1",
           subCategories: []),
       Category(
-          icon: Icon(Icons.ac_unit),
+          id: 2,
           name: "Item Name",
           price: 10000,
           imgName: "assets/Chair2",
           subCategories: []),
       Category(
-          icon: Icon(Icons.ac_unit),
+          id: 3,
           name: "Item Name",
           price: 3000,
           imgName: "assets/Chair3",
           subCategories: []),
       Category(
-          icon: Icon(Icons.ac_unit),
+          id: 4,
           name: "Item Name",
           price: 1000,
           imgName: "assets/Chair4",
